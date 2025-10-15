e3216a2b7bf0e359b7271ff0bad550079b005c0f
putrimaulidiayusuf <putrimaulidiayusuff@gmail.com>
2025-10-15 11:42:55 +0700

    Verify and register all resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDetailController;
+
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
+Route::resource('Customers', CustomerController::class);
+Route::resource('orders', OrderController::class);
+Route::resource('order_details', OrderDetailController::class);
 
 Route::get('/', function () {
     return view('welcome');
